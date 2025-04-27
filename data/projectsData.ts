42ddcecae5aceb5d8d0b9204e812552d281168e3
KS <37227406+Kausi-hub@users.noreply.github.com>
2025-04-27 12:24:34 -0400

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -10,14 +10,14 @@ const projectsData: Project[] = [
     title: 'A Family Calendar',
     description: `What if you could sync up calindars of your family and generate
     conflicts and reminders every week?`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    imgSrc: '/static/images/calendarapp.png',
+    href: '',
   },
   {
     title: 'Remote 3-d Printing',
     description: `A project to make 3-d printing affordable for hobbyists. Manage your 3-d printing jobs remotely with as minimum as $250 investment in accessories and a tiny bit of interest in electronics and programming`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: '/static/images/3dprinting.jpg',
+    href: '',
   },
 ]
 
