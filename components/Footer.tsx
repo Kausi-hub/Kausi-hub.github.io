60dcaca4cb0605b5a7c0c05aee33755a8f75b92b
KS <37227406+Kausi-hub@users.noreply.github.com>
2025-05-01 18:53:36 -0400

    Update Footer.tsx
    Signed-off-by: KS <37227406+Kausi-hub@users.noreply.github.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,9 +20,11 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/Kausi-hub/Kausi-hub.github.io">
-            Tailwind Nextjs Theme
-          </Link>
+          <div>{siteMetadata.author}</div>
+          <div>{` • `}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` • `}</div>
+          <Link href="https://kausi-hub.github.io/about/">Kausi's Blog</Link>
         </div>
       </div>
     </footer>
