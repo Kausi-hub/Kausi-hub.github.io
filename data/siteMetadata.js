0bfc89b1b58b39292e17fa98723259547879491c
KS <37227406+Kausi-hub@users.noreply.github.com>
2025-05-09 17:53:56 -0400

    env config update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,12 +63,8 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      //repo: 'Kausi-hub/Kausi-hub.github.io',
-      //repositoryId: 'R_kgDOOgIuZQ',
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      //category: 'General',
-      //categoryId: 'DIC_kwDOOgIuZc4Cp92y',
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
       mapping: 'pathname', // supported options: pathname, url, title
