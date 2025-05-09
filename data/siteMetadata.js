ba2f7942b0b810c95859877e58c116de3d5ca454
KS <37227406+Kausi-hub@users.noreply.github.com>
2025-05-09 18:21:08 -0400

    env test

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,24 +63,16 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO || '', // embed value
+      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID || '',
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY || '',
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID || '',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
+       metadata: '0',
       theme: 'light',
-      // theme when dark mode
       darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
     },
   },
@@ -99,5 +91,10 @@ const siteMetadata = {
     // },
   },
 }
-
+  console.log('Giscus env loaded:', {
+  repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+  repoId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+  category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+  categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+})
 module.exports = siteMetadata
