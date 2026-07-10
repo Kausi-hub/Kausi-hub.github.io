ffd2896213a3babf91356e70c0973114eac59932
KS <37227406+Kausi-hub@users.noreply.github.com>
2026-07-09 21:17:56 -0400

    layout syntax

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -127,7 +127,9 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   </div>
                 )}
                 <div className="divide-gray-200 text-sm leading-5 font-medium xl:col-start-1 xl:row-start-2 xl:divide-y dark:divide-gray-700">
-                  <h2 className="text-xs font-semibold tracking-wide uppercase dark:text-gray-400">Share</h2>
+                  <h2 className="text-xs font-semibold tracking-wide uppercase dark:text-gray-400">
+                    Share
+                  </h2>
 
                   <div className="mt-2">
                     <ShareButton title={title} />
